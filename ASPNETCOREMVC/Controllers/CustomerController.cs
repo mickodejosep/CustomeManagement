d0be6a4816df899dc0d578597f3d3913b72ae170
mickodejosep <mickodejosep20@gmail.com>
2022-11-12 07:55:17 +0800

    Add project files.

---
+++ ASPNETCOREMVC/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASPNETCOREMVC.Models;
+
+namespace ASPNETCOREMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private static List<Customer> customerVM = new List<Customer>();
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Customer Details";
+
+            //Customer customer = new Customer()
+            //{
+            //    Id = 1,
+            //    CustomerName = "Micko De Josep",
+            //    CustomerAge = 25
+            //};
+           
+
+            return View(customerVM);
+        }
+
+        public IActionResult Create()
+        {
+            var CustomerVM = new Customer();
+            return View(CustomerVM);
+        }
+
+        public IActionResult CreateCustomer(Customer customerViewModel)
+        {
+            customerVM.Add(customerViewModel);
+            //return View("Index");
+            return RedirectToAction(nameof(Index));
+        }
+        
+    }
+}
---
+++ ASPNETCOREMVC/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPNETCOREMVC.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? CustomerName { get; set; }
+        public int CustomerAge { get; set; }
+    }
+}
